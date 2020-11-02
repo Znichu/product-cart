444d1f9d4f5ad76ba0e8ed5ab024169c2e73a602
Sergey Neplashov <znichu1989@gmail.com>
2020-11-02 16:49:46 +0300

    fix firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
-
 let config = {
-    apiKey: "AIzaSyBDcoM7XiqvqbYd8JnCxq1EOuclfFqx1NM",
-    authDomain: "react-pizza-765eb.firebaseapp.com",
-    databaseURL: "https://react-pizza-765eb.firebaseio.com",
-    projectId: "react-pizza-765eb",
-    storageBucket: "react-pizza-765eb.appspot.com",
-    messagingSenderId: "809954416511",
-    appId: "1:809954416511:web:e8373b4fc89a06c4542c4a"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
 };
 
 
